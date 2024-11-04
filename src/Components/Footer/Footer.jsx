8f872b80f16ef762fb85839d35d77e8ec2839f30
gr99murad <golamrabbanimurad@gmail.com>
2024-11-04 22:20:39 +0600

    Added json data and added navbar & Footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+const Footer = () => {
+  return (
+    <div>
+
+<footer className="footer grid grid-flow-row  bg-base-200 rounded p-10">
+ 
+  <div className="mx-auto">
+    <h1 className="mx-auto text-5xl">Gadget Heaven</h1>
+    <p className="text-center mt-3">Leading the way in cutting-edge technology and innovation.</p>
+   </div>
+  
+   <footer className="footer mx-auto p-10">
+  <nav>
+    <h6 className="footer-title">Services</h6>
+    <a className="link link-hover">Branding</a>
+    <a className="link link-hover">Design</a>
+    <a className="link link-hover">Marketing</a>
+    <a className="link link-hover">Advertisement</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Company</h6>
+    <a className="link link-hover">About us</a>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Jobs</a>
+    <a className="link link-hover">Press kit</a>
+  </nav>
+  <nav>
+    <h6 className="footer-title">Legal</h6>
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+</footer>
+</footer>
+
+
+</div>
+    
+  );
+};
+
+export default Footer;
--- src/Components/MainLayouts/MainLayouts.jsx
+++ src/Components/MainLayouts/MainLayouts.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
 
 const MainLayouts = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
